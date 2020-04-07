23f7bdbc0a7d2603156415c630368cd4181ad3eb
Ogenrwot Aaron <ogenrwotaron@gmail.com>
2020-04-07 23:04:04 +0300

    Corrected lint Errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,5 @@
-//Input data structured as
-/*{
+// Input data structured as
+/* {
     region: {
         name: "Africa",
         avgAge: 19.7,
@@ -11,9 +11,9 @@
     reportedCases: 674,
     population: 66622705,
     totalHospitalBeds: 1380614
-}*/
+} */
 
-//Output data structured as
+// Output data structured as
 /*
 {
     data: {}, // the input data you got
@@ -22,52 +22,50 @@
 }
 */
 
-const covid19ImpactEstimator = (data) => {
+// Returns the infectionsByRequestTime value
+const infections = (currentlyInfected, timeToElapse, periodType) => {
+  let infectionsByRequestTime = null;
+  let days;
+  switch (periodType) {
+    case 'days':
+      infectionsByRequestTime = currentlyInfected * (2 ** Math.trunc(timeToElapse / 3));
+      break;
+    case 'weeks':
+      days = timeToElapse * 7;
+      infectionsByRequestTime = currentlyInfected * (2 ** Math.trunc(days / 3));
+      break;
+    case 'months':
+      days = timeToElapse * 30;
+      infectionsByRequestTime = currentlyInfected * (2 ** Math.trunc(days / 3));
+      break;
+    default:
+      infectionsByRequestTime = currentlyInfected * (2 ** Math.trunc(timeToElapse / 3));
+      break;
+  }
 
-    const {reportedCases,timeToElapse,periodType}=data;
-
-    const impact={
-        currentlyInfected:reportedCases*10,
-        get infectionsByRequestTime(){
-            return infections(this.currentlyInfected,timeToElapse,periodType)
-        }
-    }
-    const severeImpact={
-        currentlyInfected:reportedCases*50,
-        get infectionsByRequestTime(){
-            return infections(this.currentlyInfected,timeToElapse,periodType)
-        }
-    }
-
-    return {
-        data,
-        impact,
-        severeImpact
-    }
+  return infectionsByRequestTime;
 };
 
-//Returns the infectionsByRequestTime value
-const infections=(currentlyInfected,timeToElapse,periodType)=>{
-    let infectionsByRequestTime=null;
-    let days;
-    switch (periodType) {
-        case 'days':
-            infectionsByRequestTime=currentlyInfected*(Math.pow(2,Math.trunc(timeToElapse/3)));
-            break;
-        case 'weeks':
-            days=timeToElapse*7
-            infectionsByRequestTime=currentlyInfected*(Math.pow(2,Math.trunc(days/3)));
-            break;
-        case 'months':
-            days=timeToElapse*30
-            infectionsByRequestTime=currentlyInfected*(Math.pow(2,Math.trunc(days/3)));
-            break;
-        default:
-            infectionsByRequestTime=currentlyInfected*(Math.pow(2,Math.trunc(timeToElapse/3)));
-            break;
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases, timeToElapse, periodType } = data;
+
+  const impact = {
+    currentlyInfected: reportedCases * 10,
+    get infectionsByRequestTime() {
+      return infections(this.currentlyInfected, timeToElapse, periodType);
     }
+  };
+  const severeImpact = {
+    currentlyInfected: reportedCases * 50,
+    get infectionsByRequestTime() {
+      return infections(this.currentlyInfected, timeToElapse, periodType);
+    }
+  };
 
-    return infectionsByRequestTime
-}
-
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 export default covid19ImpactEstimator;
